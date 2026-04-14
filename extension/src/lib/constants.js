97793ecc2948211f29d32747229b0a8ef8a95cbe
Axel mac <axelhernandezperez23@gmail.com>
2026-04-13 22:06:27 -0600

    config env variable fallback chnages

--- extension/src/lib/constants.js
+++ extension/src/lib/constants.js
@@ -2,4 +2,4 @@
  * Default API base for production.
  * Uses the Render default domain wildcard allowed in manifest host_permissions.
  */
-export const DEFAULT_API_BASE = "https://apa-asistente-api.onrender.com";
+export const DEFAULT_API_BASE = "https://express-deploy-test-kbkd.onrender.com";
