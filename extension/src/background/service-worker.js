be182b62e2f8ee84618748426d5feea102b52c91
Axel mac <axelhernandezperez23@gmail.com>
2026-05-07 12:09:21 -0600

    auto updater test dafne device

--- extension/src/background/service-worker.js
+++ extension/src/background/service-worker.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("[APA Asistente] Extension installed");
 });
 
-console.log("testing auto update script locally, 03 - CMD only test");
+console.log("testing auto update script locally, 04 - CMD only test, test dafne device");
 
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   if (message?.type === MESSAGE_TYPES.LOOKUP_ACCOUNT && message.clave) {
